fcb6695abc3ba5cc760ed7f1ccab2c2eda0d8d4a
EspinacaLaser <ignacioramosmartin18@gmail.com>
2024-12-02 13:08:28 +0100

    commit5

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
 
 export default defineConfig({
+  root: "./src", // Cambia la raíz del proyecto a "src"
   build: {
-    root:"./src",
-    outDir: "./docs",
-    rollupOptiones: {
-      input: {
-        main: resolve("src", "index.html"),
-      },
-    },
+    outDir: "../docs", // Los archivos construidos irán a la carpeta "docs"
   },
-  base: "./",
 });
