cee1a23faa8935c83be0a191891335584e3805e9
EspinacaLaser <ignacioramosmartin18@gmail.com>
2024-12-02 18:34:05 +0100

    cmbio2

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,18 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use './src/styles/_variables.scss' as *;`, // Asegúrate de usar la ruta absoluta
+  root: "./src", // Cambia la raíz del proyecto a "src"
+  build: {
+    outDir: "../docs", // Directorio de salida para GitHub Pages
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "./src/index.html"), // Página principal
+        about: resolve(__dirname, "./src/about.html"), // Otra página
+        contact: resolve(__dirname, "./src/contact.html"), // Otra página
+        // Añade más entradas si es necesario
       },
     },
   },
-  root: "./src",
-  build: {
-    outDir: "../docs",
-  },
+  base: "./", // Usar rutas relativas para GitHub Pages
 });
