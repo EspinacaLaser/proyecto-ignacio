943c53a53da720f868311886381f2c7a1c0656ce
EspinacaLaser <ignacioramosmartin18@gmail.com>
2024-12-03 10:31:06 +0100

    commit seguro

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,14 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig ({
+  base: "",
   build: {
     root: "./src", //cambia la raiz del proyecto a src
     outDir: "./docs", // Cambia el directorio de salida
     rollupOptions: {
       input: {
-        main: resolve("src", "index.html"),
+        main: resolve("index.html"),
       },
     },
     },
-    base: "./",
 });
